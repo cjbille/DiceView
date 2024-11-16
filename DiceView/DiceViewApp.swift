1b74649e982d42b533b76c023b395f86c2093f3a
Chris Bille <cbille1@icloud.com>
2024-11-16 09:58:30 -0500

    Initial Commit

---
+++ DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Christopher Bille on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
